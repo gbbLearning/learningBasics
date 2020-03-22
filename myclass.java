1ef22ccf800a5f022b0cb84521131169dd1ced7b
Gonzalo Benítez Bueno <gonsama@Gonzalos-MacBook-Pro.local>
2020-03-22 20:10:59 +0100

    commit desde VS

--- myclass.java
+++ myclass.java
@@ -2,7 +2,7 @@ public class myclass{
 
     public void PrintSomething()
     {
-        System.out.println("Pintando algo desde una clase");
+        System.out.println("Pintando algo desde una clase modificada");
     } 
 
 }
